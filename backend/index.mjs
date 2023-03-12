ec229ce2863d86ffb72d155df7de7692a810ead6
Mohammed Shousha <mshousha29@gmail.com>
2023-03-12 14:35:13 +0200

    backend-2

--- backend/index.mjs
+++ backend/index.mjs
@@ -5,6 +5,7 @@ import { MongoClient, ObjectId } from 'mongodb'
 
 dotenv.config()
 
+
 const app = express()
 const { MONGO_URI, PORT } = process.env
 
@@ -14,11 +15,28 @@ const db = client.db('Banking-System')
 const customers = db.collection('Customers')
 const transfers = db.collection('Transfers')
 
+
+const idToName = async (transfers) => {
+   const result = await customers.find().toArray()
+
+   const idToName = {}
+   result.forEach((customer) => {
+      idToName[customer._id] = customer.name
+   })
+
+   transfers.forEach((transfer) => {
+      transfer.from = idToName[transfer.from]
+      transfer.to = idToName[transfer.to]
+   })
+
+   return transfers
+}
+
 app.use(express.json())
 app.use(cors())
 
 
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
    res.send('Banking System API')
 })
 
@@ -40,46 +58,75 @@ app.get('/', (req, res) => {
 
 app.post('/addTransfer', async (req, res) => {
    const { fromId, toId, amount } = req.body
+   
+   if(!fromId || !toId || !amount) {
+      return res.send({ error: 'Invalid Data' })
+   }
 
    const from = await customers.findOne({ _id: new ObjectId(fromId) })
 
-   if (from.balance < amount) {
-      res.send({ error: 'Insufficient Balance' })
-   } else {
-      await customers.updateOne(
-         { _id: new ObjectId(fromId) },
-         { $inc: { balance: -amount } }
-      )
-      await customers.updateOne(
-         { _id: new ObjectId(toId) },
-         { $inc: { balance: amount } }
-      )
-      const result = await transfers.insertOne({
-         from: fromId,
-         to: toId,
-         amount,
-         date: new Date(),
-      })
-      res.json(result)
+   if (!from) {
+      return res.send({ error: 'Account Not Found' })
    }
+
+   if (from.balance < amount) {
+      return res.send({ error: 'Insufficient Balance' })
+   }
+
+   await customers.updateOne(
+      { _id: new ObjectId(fromId) },
+      { $inc: { balance: -amount } }
+   )
+   await customers.updateOne(
+      { _id: new ObjectId(toId) },
+      { $inc: { balance: amount } }
+   )
+   const result = await transfers.insertOne({
+      from: fromId,
+      to: toId,
+      amount,
+      date: new Date(),
+   })
+   return res.json(result)
 })
 
-app.get('/customers', async (req, res) => {
-   const result = await customers.find({}).toArray()
-   res.json(result)
+app.get('/customers', async (_, res) => {
+   const result = await customers.find().toArray()
+   return res.json(result)
 })
 
-app.get('/transfers', async (req, res) => {
-   const result = await transfers.find({}).toArray()
-   res.json(result)
+app.get('/transfers', async (_, res) => {
+   const result = await transfers.find().toArray()
+
+   const allTransfers = await idToName(result)
+
+   allTransfers.sort((a, b) => {
+      return new Date(b.date) - new Date(a.date)
+   })
+
+   return res.json(allTransfers)
 })
 
 app.get('/customers/:id', async (req, res) => {
    const { id } = req.params
    const result = await customers.findOne({ _id: new ObjectId(id) })
-   res.json(result)
+   return res.json(result)
 })
 
+app.get('/customerTransfers/:id', async (req, res) => {
+   const { id } = req.params
+   const transfersFrom = await transfers.find({ from: id }).toArray()
+   const transfersTo = await transfers.find({ to: id }).toArray()
+   const allTransfers = [...transfersFrom, ...transfersTo]
+
+   const userTransfers = await idToName(allTransfers)
+
+   userTransfers.sort((a, b) => {
+      return new Date(b.date) - new Date(a.date)
+   })
+
+   return res.json(userTransfers)
+})
 
 
 const port = PORT || 5000
